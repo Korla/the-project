f82f0fe156d12072e0c22eec6f0bfa8a57ef7d54
korla <bengtsson.johannes@gmail.com>
2019-05-01 13:10:39 +0200

    Fix default routing on netlify

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <div>
         <TopNav />
-        <Route exact path="/" component={Home} />
+        <Route path="/" component={Home} />
         <Route exact path="/details/:id" component={Details} />
       </div>
     </BrowserRouter>
